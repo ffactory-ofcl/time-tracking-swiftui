81f454e99b9f6f31577376d0c816b862d6e44e21
Filippo Orru <filippoorru@hotmail.com>
2023-01-03 11:52:20 +0700

    Initial Commit

---
+++ time tracking/time_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_trackingApp.swift
+//  time tracking
+//
+//  Created by Filippo Orru on 03.01.23.
+//
+
+import SwiftUI
+
+@main
+struct time_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
